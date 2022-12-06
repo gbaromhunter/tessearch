9c6713bff4bd1f194bbeb7914b49f17dc2a1e65a
Gbaromhunter <gbaromshunter@hotmail.com>
2022-12-06 15:47:09 +0200

    make_files now it's extrapolate and returns a dictionary, optionally writing files

--- tessearch.py
+++ tessearch.py
@@ -15,16 +15,22 @@ def remove_ext(string):
     return string[:string.rindex(".")]
 
 
-def make_files(join=False):
+def get_filename(string):
+    return string[string.rindex("/")+1:string.rindex(".")]
+
+
+def extrapolate(join=False, write=False):
     """if join is True it extrapolates all the text from all the images in a single text file
     otherwise it creates a text file for each image with its file name"""
-    texts = []
+    texts = {}
     for image in images:
         img = Image.open(image)
         filepath = "all_texts.txt" if join else f"{remove_ext(image)}.txt"
         textrev = pytesseract.image_to_string(img)
         text = textrev[:-1]
-        texts.append(text)
+        texts[get_filename(image)] = text
         print(f"{image} contains the following text: \n {text}")
-        with open(filepath, 'a' if join else 'w') as f:
-            f.write(text)
+        if write:
+            with open(filepath, 'a' if join else 'w') as f:
+                f.write(text)
+    return texts
