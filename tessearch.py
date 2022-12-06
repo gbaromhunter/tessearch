fb908225655a87f804e8274e33e570841ee365e7
Gbaromhunter <gbaromshunter@hotmail.com>
2022-12-06 15:20:35 +0200

    removed previous possible bug from make_files

--- tessearch.py
+++ tessearch.py
@@ -11,20 +11,20 @@ folder_dir = r"C:/tessearch/images"
 images = [f"{folder_dir}/{image}" for image in os.listdir(folder_dir)]
 
 
+def remove_ext(string):
+    return string[:string.index(".")]
+
+
 def make_files(join=False):
     """if join is True it extrapolates all the text from all the images in a single text file
     otherwise it creates a text file for each image with its file name"""
     texts = []
     for image in images:
         img = Image.open(image)
-        # if file extension has more than 3 character the output will be unexpected.
-        filepath = "all_texts.txt" if join else f"{image[:-4]}.txt"
+        filepath = "all_texts.txt" if join else f"{remove_ext(image)}.txt"
         textrev = pytesseract.image_to_string(img)
         text = textrev[:-1]
         texts.append(text)
         print(f"{image} contains the following text: \n {text}")
         with open(filepath, 'a' if join else 'w') as f:
             f.write(text)
-
-
-make_files()
