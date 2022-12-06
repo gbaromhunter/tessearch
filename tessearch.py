c118b024f4959d64cb8084f4e32a01845f964a61
Gbaromhunter <gbaromshunter@hotmail.com>
2022-12-06 13:51:52 +0200

    found a possible bug in the code

--- tessearch.py
+++ tessearch.py
@@ -12,9 +12,12 @@ images = [f"{folder_dir}/{image}" for image in os.listdir(folder_dir)]
 
 
 def make_files(join=False):
+    """if join is True it extrapolates all the text from all the images in a single text file
+    otherwise it creates a text file for each image with its file name"""
     texts = []
     for image in images:
         img = Image.open(image)
+        # if file extension has more than 3 character the output will be unexpected.
         filepath = "all_texts.txt" if join else f"{image[:-4]}.txt"
         textrev = pytesseract.image_to_string(img)
         text = textrev[:-1]
@@ -22,5 +25,6 @@ def make_files(join=False):
         print(f"{image} contains the following text: \n {text}")
         with open(filepath, 'a' if join else 'w') as f:
             f.write(text)
+
 
 make_files()
